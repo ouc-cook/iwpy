1652ba7f9d0721d02ffbfe2430e13c27bba1a212
olavobm <olavobm@gmail.com>
2018-03-02 10:07:59 -0800

    added docstring.

--- iwVisual/visualDefs.py
+++ iwVisual/visualDefs.py
@@ -1,8 +1,11 @@
 import numpy as np
 #import pandas as pd
 
-def wavedomain(xlim, ylim, zlim = np.NaN, tlim = np.NaN):
-    """ 
+def wavedomain(xlim, ylim = np.NaN, zlim = np.NaN, tlim = np.NaN):
+    """ Create a dictionary defining the boundaries
+    of the domain. This can then be accessed by higher
+    level functions to create the visualization, using
+    the specified domain.
     """
 
     domainInfo = {}
