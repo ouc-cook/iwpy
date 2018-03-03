21a716c7fb925bea835b5183ff47cca4571815b8
olavobm <olavobm@gmail.com>
2018-03-03 00:45:23 -0800

    added wavegrid.

--- iwVisual/visualDefs.py
+++ iwVisual/visualDefs.py
@@ -1,13 +1,14 @@
 import numpy as np
 #import pandas as pd
 
-def wavedomain(xlim, ylim = np.NaN, zlim = np.NaN, tlim = np.NaN):
+# -----------------------------
+def wavedomain(tlim, xlim = np.NaN, ylim = np.NaN, zlim = np.NaN):
     """ Create a dictionary defining the boundaries
     of the domain. This can then be accessed by higher
     level functions to create visualizations on the
     specified domain.
     """
-    
+
     domainBndry = {}
     domainBndry['xlim'] = xlim
     domainBndry['ylim'] = ylim
@@ -15,3 +16,12 @@ def wavedomain(xlim, ylim = np.NaN, zlim = np.NaN, tlim = np.NaN):
     domainBndry['tlim'] = tlim
 
     return domainBndry
+
+# -----------------------------
+def wavegrid(tg, xg = np.NaN, yg = np.NaN, zg = np.NaN):
+
+    domainGrid = {}
+    domainGrid['tg'] = tg
+    domainGrid['xg'] = xg
+    domainGrid['yg'] = yg
+    domainGrid['zg'] = zg
