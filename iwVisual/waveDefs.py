bece6b36729e6d473544393b0da755aa9b52264b
olavobm <olavobm@gmail.com>
2018-03-04 10:29:24 -0800

    changed class and function names.

--- iwVisual/waveDefs.py
+++ iwVisual/waveDefs.py
@@ -3,38 +3,40 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # ------------------------------------
-class Wave1d:
+class PlaneWave1d:
 
     #
     a = 1
-    k = 1
-    wvfreq = 1
-    phi0 = 1
+    phi0 = 0
 
     #
-    xg = 1
-    tg = 1
+    k = np.nan
+    wvfreq = np.nan
+
+    #
+    xg = np.nan
+    tg = np.nan
 
     # ------------------------------------
+    # Create array with n lines where n = len(t)
     def wave1d(self, t = tg):
-        # Create array with n lines where n = len(t)
         return self.a * np.cos(self.k * self.xg +
                                self.wvfreq * t +
                                self.phi0)
 
-# ------------------------------------
-def plotwave1d(i):
-#    ax.clear()
-    ax.plot(wave.domainGrid['xg'], wave1d(t[i]))
+    # ------------------------------------
+    def plotwave(i):
+    #    ax.clear()
+        ax.plot(wave.domainGrid['xg'], wave1d(t[i]))
 
-# ------------------------------------
-def planewave1d(fig):
-    animation.FuncAnimation(fig,
-                            plotwave1d,
-                            frames = range(1, len(test_data)),
-                            interval = 1000, repeat = False)
+    # ------------------------------------
+    def moviewave(fig):
+        animation.FuncAnimation(fig,
+                                plotwave,
+                                frames = range(1, len(test_data)),
+                                interval = 1000, repeat = False)
 
-# ------------------------------------
-def planewave1d(wave):
-    for i  in wave.domainGrid['tg']:
-        plot(wave.domainGrid['xg'], wave.wave1d(i))
+    # ------------------------------------
+    def planewave1d(wave):
+        for i  in wave.domainGrid['tg']:
+            plot(wave.domainGrid['xg'], wave.wave1d(i))
