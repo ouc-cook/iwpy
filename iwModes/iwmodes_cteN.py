a6c0c55d2b7eda9d436369bcebaf02ab62b667b4
olavobm <olavobm@gmail.com>
2018-03-08 23:06:13 -0800

    started to add a few functions (but still need to make them functional).

--- iwModes/iwmodes_cteN.py
+++ iwModes/iwmodes_cteN.py
@@ -1,12 +1,29 @@
-# Functions
+# Functions to make calculations for
+# individual standard oceanic vertical modes.
 #
 # Olavo Badaro Marques
 
 import numpy as np
 import matplotlib.pyplot as plt
+# need to import other internal wave files!!!
 
 # ----------------------------------------
+def horizwvlen(wvfreq, N, f0, D, nmd):
+
+    m = nmd * np.pi / D
+    k = m2k(m, wvfreq, f0, N)
+
+    l = 2 * np.pi / k
+    return l
 
 # ----------------------------------------
+def cn2cp(cn, wvfreq, lat):
+    factor = wvfreq / (sqrt(wvfreq**2 - f0**2))
+    cp = cn * times_factor
+    return cp
 
 # ----------------------------------------
+def cn2cg(cn, wvfreq, lat):
+    factor = wvfreq / (sqrt(wvfreq**2 - f0**2))
+    cg = cn / factor
+    return cg
