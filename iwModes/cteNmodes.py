ff05c2e5ad25cca2edc3fb0317c0a5df077082b9
olavobm <olavobm@gmail.com>
2018-03-08 17:10:04 -0800

    initial commit.

---
+++ iwModes/cteNmodes.py
@@ -0,0 +1,12 @@
+# Functions
+#
+# Olavo Badaro Marques
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ----------------------------------------
+
+# ----------------------------------------
+
+# ----------------------------------------
